ee49d2cad7491709330743944689efec9c4bfc8e
xale <alexmheinz@gmail.com>
2023-10-09 22:11:57 -0700

    Renamed main plugin class.

---
+++ PressureVessel.cs
@@ -0,0 +1,25 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System.Reflection;
+
+namespace xale.Subnautica.PressureVessel;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency("com.snmodding.nautilus")]
+public class PressureVessel : BaseUnityPlugin
+{
+    public new static ManualLogSource Logger { get; private set; }
+
+    private static Assembly Assembly { get; } = Assembly.GetExecutingAssembly();
+
+    private void Awake()
+    {
+        // set project-scoped logger instance
+        Logger = base.Logger;
+
+        // register harmony patches, if there are any
+        Harmony.CreateAndPatchAll(Assembly, $"{MyPluginInfo.PLUGIN_GUID}");
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
