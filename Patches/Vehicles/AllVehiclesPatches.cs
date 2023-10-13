7dedb9e2817659d618f63548c00479353067c688
xale <alexmheinz@gmail.com>
2023-10-12 22:08:20 -0700

    First working patch! Disallow vehicle exit if below safe depth.

---
+++ Patches/Vehicles/AllVehiclesPatches.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using xale.Subnautica.PressureVessel.Config;
+
+namespace xale.Subnautica.PressureVessel.Patches.Vehicles;
+
+[HarmonyPatch(typeof(Vehicle))]
+internal static class AllVehiclesPatches
+{
+    [HarmonyPatch(nameof(Vehicle.GetAllowedToEject)), HarmonyPrefix]
+    private static bool GetAllowedToEject_Prefix(Vehicle __instance, ref bool __result)
+    {
+        ErrorMessage.AddDebug("GetAllowedToEject_Prefix");
+        bool lockHatches = PressureVesselOptions.get().lockHatches;
+        ErrorMessage.AddDebug($"lockHatches: {lockHatches}");
+
+        if (!lockHatches) { return true; }
+
+        Vehicle vehicle = __instance;
+
+        int currentDepth;
+        vehicle.GetDepth(out currentDepth, out _);
+
+        ErrorMessage.AddDebug($"currentDepth: {currentDepth}");
+
+        int safeDepth = PressureVesselConfig.SafeDepth.Value;
+
+        ErrorMessage.AddDebug($"safeDepth: {safeDepth}");
+
+        if (currentDepth <= PressureVesselConfig.SafeDepth.Value) { return true; }
+
+        ErrorMessage.AddError("Cannot open hatch; pressure is too high");
+
+        __result = false;
+        return false;
+    }
+}
