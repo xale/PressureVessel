e80a66bbf4adc03fe8521d9fcd262b813a73cb22
xale <alexmheinz@gmail.com>
2023-10-16 22:51:25 -0700

    Added the ability to open vehicle storage and module panels from inside the vehicle.

---
+++ Patches/PlayerPatches.cs
@@ -0,0 +1,68 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using UnityEngine;
+using xale.Subnautica.PressureVessel.Config;
+
+namespace xale.Subnautica.PressureVessel.Patches;
+
+[HarmonyPatch(typeof(Player))]
+internal class PlayerPatches
+{
+    [HarmonyPatch(nameof(Player.Update)), HarmonyPostfix]
+    public static void Update_Postfix(Player __instance)
+    {
+        if (!AvatarInputHandler.main.IsEnabled()) { return; }
+
+        Vehicle currentVehicle = __instance.currentMountedVehicle;
+
+        if (currentVehicle == null || currentVehicle.ignoreInput) { return; }
+
+        if (Input.GetKeyDown(PressureVesselOptions.get().openVehicleInventoryKey))
+        {
+            DebugMessages.Show("keyDown: openVehicleInventoryKey");
+
+            Exosuit prawn = (currentVehicle as Exosuit);
+            if (prawn != null)
+            {
+                OpenPrawnStorage(prawn);
+                return;
+            }
+
+            SeaMoth seamoth = (currentVehicle as SeaMoth);
+            if (seamoth != null)
+            {
+                OpenSeamothStorage(seamoth);
+                return;
+            }
+
+            DebugMessages.Show($"Unknown vehicle type: ${currentVehicle}");
+            return;
+        }
+
+        if (Input.GetKeyDown(PressureVesselOptions.get().openVehicleModulesKey))
+        {
+            DebugMessages.Show("keyDown: openVehicleModulesKey");
+            currentVehicle.upgradesInput.OpenFromExternal();
+        }
+    }
+
+    private static void OpenPrawnStorage(Exosuit prawn)
+    {
+        Inventory.main.ClearUsedStorage();
+        Inventory.main.SetUsedStorage(prawn.storageContainer.container);
+        Player.main.GetPDA().Open(PDATab.Inventory);
+    }
+
+    private static void OpenSeamothStorage(SeaMoth seamoth)
+    {
+        List<IItemsContainer> storages = new List<IItemsContainer>();
+        seamoth.GetAllStorages(storages);
+
+        Inventory.main.ClearUsedStorage();
+        foreach (IItemsContainer storage in storages)
+        {
+            Inventory.main.SetUsedStorage(storage, /* append= */ true);
+        }
+        Player.main.GetPDA().Open(PDATab.Inventory);
+    }
+}
