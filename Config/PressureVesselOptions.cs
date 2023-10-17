26e53e3c2d2aa5fda385e483daea1555dfea51ca
xale <alexmheinz@gmail.com>
2023-10-16 18:15:58 -0700

    Added keybinds to open vehicle storage and module panels to `PressureVesselOptions`.
    (Actions are not yet functional; adds settings only.)

--- Config/PressureVesselOptions.cs
+++ Config/PressureVesselOptions.cs
@@ -1,6 +1,7 @@
 ﻿using Nautilus.Handlers;
 using Nautilus.Json;
 using Nautilus.Options.Attributes;
+using UnityEngine;
 
 namespace xale.Subnautica.PressureVessel.Config;
 
@@ -22,4 +23,14 @@ internal class PressureVesselOptions : ConfigFile
         "Lock hatches",
         Tooltip = "If checked, you cannot leave a vehicle or habitat below safe diving depth")]
     public bool lockHatches = true;
+
+    [Keybind(
+        "Open vehicle inventory",
+        Tooltip = "Opens the inventory of the vehicle you are currently piloting")]
+    public KeyCode openVehicleInventoryKey = KeyCode.LeftBracket;
+
+    [Keybind(
+        "Open vehicle modules",
+        Tooltip = "Open the module upgrade panel for the vehicle you are currently piloting")]
+    public KeyCode openVehicleModulesKey = KeyCode.RightBracket;
 }
