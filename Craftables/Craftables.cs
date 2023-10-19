0aea47085deda56ca9e9a3522035b0b0cbc7f5d6
xale <alexmheinz@gmail.com>
2023-10-19 16:48:25 -0700

    Registered ROV vehicle-upgrade module for use in-game.

--- Craftables/Craftables.cs
+++ Craftables/Craftables.cs
@@ -14,6 +14,7 @@ internal static class Craftables
     public static void RegisterAll()
     {
         HabitatBuilderModule.Register();
+        ROVModule.Register();
         ScannerModule.Register();
     }
 }
