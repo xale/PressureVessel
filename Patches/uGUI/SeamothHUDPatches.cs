a68285b765537754cfee093c1ce537f6bce11624
xale <alexmheinz@gmail.com>
2023-10-25 16:22:37 -0700

    Hid the Seamoth HUD when piloting an `ROV`.

---
+++ Patches/uGUI/SeamothHUDPatches.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using xale.Subnautica.PressureVessel.Behaviours;
+
+namespace xale.Subnautica.PressureVessel.Patches.uGUI;
+
+[HarmonyPatch(typeof(uGUI_SeamothHUD))]
+internal static class SeamothHUDPatches
+{
+    [HarmonyPatch(nameof(uGUI_SeamothHUD.Update)), HarmonyPostfix]
+    internal static void Update_Postfix(uGUI_SeamothHUD __instance)
+    {
+        if (!__instance.root.activeSelf) { return; }
+
+        // Hide vehicle HUDs when piloting an ROV.
+        ROV currentRov = Player.main.GetVehicle()?.GetComponent<ROVDocking>()?.lastDocked;
+        __instance.root.SetActive((currentRov == null) || !currentRov.active);
+    }
+}
